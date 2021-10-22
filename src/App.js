76f6077170e0ed8fe574e9a782d0c801fa271878
Satyajit@Codys <satyajitdebnath87@gmail.com>
2021-10-22 17:10:58 +0530

    welcome msg added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import SignIn from './pages/SignIn';
 import Post from './pages/Post';
 import Profile from './pages/Profile';
+import Homepage from './pages/Homepage';
 import {Route,Switch,Redirect} from 'react-router-dom';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
     <Route path='/signin' exact component={SignIn}/>
     <Route path='/posts' exact component={()=> isloggedIn?  <Post />:<Redirect to="/signin" />}/>
     <Route path='/user/:id' exact component={()=> isloggedIn?  <Profile />:<Redirect to="/signin" />}/>
-
+    <Route path='/' exact component={Homepage}/>
     </Switch>
    
     </>
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home" align="center">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="https://media.istockphoto.com/photos/on-colourful-speech-bubbles-picture-id180819641?b=1&k=20&m=180819641&s=170667a&w=0&h=CX51cRVofQl95e_cu9Bfy5PLZQ1WdsqmJ-NCFzU96UI="
+              alt=""
+              align="center"
+            />
+          
+            <h1>Click on Posts to Continue</h1>
+            
+          </div>
+        
+  );
+}
+
+export default Home;
